8d1bba72df78549d86e38566a8570ca16b543d10
thomas <thomas@dooms.eu>
2021-09-13 01:03:51 +0200

    temp commit

--- base/src/components/navbar/main.rs
+++ base/src/components/navbar/main.rs
@@ -1,4 +1,5 @@
 use yew::prelude::*;
+use crate::props::Active;
 
 #[derive(Clone, Debug, Properties, PartialEq)]
 pub struct Props {
@@ -11,59 +12,47 @@ pub struct Props {
     #[prop_or_default]
     pub end: Option<Html>,
 
+    #[prop_or_default]
+    pub active: Active,
+
     #[prop_or(true)]
     pub burger: bool,
+
+    pub onclick: Callback<()>,
 }
 
-pub struct Navbar {
-    active: bool,
-}
+#[function_component(Navbar)]
+pub fn navbar(props: &Props) -> Html {
+    let classes = classes!("navbar-burger", props.active);
+    let onclick = props.onclick.reform(|_| ());
 
-impl Component for Navbar {
-    type Message = ();
-    type Properties = Props;
+    let burger = match props.burger {
+        true => html! {
+            <a role="button" class={classes} aria-label="menu" aria-expanded="false" data-target="navbar" onclick={onclick}>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        },
+        false => html! {}
+    };
 
-    fn create(_: &Context<Self>) -> Self {
-        Self { active: false }
-    }
+    html! {
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                { props.brand.clone().unwrap_or_default() }
+                { burger }
+            </div>
 
-    fn update(&mut self, _: &Context<Self>, _: Self::Message) -> bool {
-        self.active = !self.active;
-        true
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        let onclick = ctx.link().callback(|_| ());
-        let classes = classes!("navbar-burger", self.active.then(|| "is-active"));
-
-        let burger = match ctx.props().burger {
-            true => html! {
-                <a role="button" class={classes} aria-label="menu" aria-expanded="false" data-target="navbar" onclick={onclick}>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                </a>
-            },
-            false => html! {}
-        };
-
-        html! {
-            <nav class="navbar" role="navigation" aria-label="main navigation">
-                <div class="navbar-brand">
-                    { ctx.props().brand.clone().unwrap_or_default() }
-                    { burger }
+            <div id="navbar" class="navbar-menu">
+                <div class="navbar-start">
+                    { props.start.clone().unwrap_or_default() }
                 </div>
 
-                <div id="navbar" class="navbar-menu">
-                    <div class="navbar-start">
-                        { ctx.props().start.clone().unwrap_or_default() }
-                    </div>
-
-                    <div class="navbar-end">
-                        { ctx.props().end.clone().unwrap_or_default() }
-                    </div>
+                <div class="navbar-end">
+                    { props.end.clone().unwrap_or_default() }
                 </div>
-            </nav>
-        }
+            </div>
+        </nav>
     }
 }
--- custom/src/components/mod.rs
+++ custom/src/components/mod.rs
@@ -2,7 +2,7 @@ pub use center::Center;
 pub use dyn_image::DynImage;
 pub use enum_buttons::EnumButtons;
 pub use loading::Loading;
-pub use simple_field::SimpleField;
+pub use field::SimpleField;
 pub use title_hero::TitleHero;
 
 mod center;
