01933418ea98f0a811c9e6c8190a3d765b160601
thomas <thomas@dooms.eu>
2022-01-19 23:57:41 +0100

    size on enum button

--- custom/src/components/enum_buttons.rs
+++ custom/src/components/enum_buttons.rs
@@ -2,18 +2,18 @@ use strum::IntoEnumIterator;
 use yew::prelude::*;
 
 use base::elements::{Button, Buttons};
-use base::props::{Alignment, Color};
+use base::props::{Alignment, Color, Size};
 
 #[derive(Clone, Properties, PartialEq)]
 pub struct Props<T: IntoEnumIterator + ToString + Copy + PartialEq + 'static> {
-    #[prop_or_default]
-    pub children: Children,
-
     #[prop_or_default]
     pub extra: String,
 
     #[prop_or_default]
     pub alignment: Alignment,
+
+    #[prop_or_default]
+    pub size: Size,
 
     pub color: Color,
 
@@ -26,20 +26,21 @@ pub struct Props<T: IntoEnumIterator + ToString + Copy + PartialEq + 'static> {
 pub fn enum_buttons<T: IntoEnumIterator + ToString + Copy + PartialEq + 'static>(
     props: &Props<T>,
 ) -> Html {
+    let Props{extra, alignment, size, color, value, onclick} = &props;
     let button_map = |variant: T| {
-        let selected = props.value == variant;
-        let color = selected.then(|| props.color);
-        let onclick = props.onclick.reform(move |_| variant);
+        let selected = value == &variant;
+        let color = selected.then(|| color);
+        let onclick = onclick.reform(move |_| variant);
 
         html! {
-            <Button color={color} onclick={onclick} selected={selected}>
-                {variant.to_string()}
+            <Button color={color.cloned()} onclick={onclick} selected={selected} size={*size}>
+                { variant.to_string() }
             </Button>
         }
     };
 
     html! {
-        <Buttons addons=true alignment={props.alignment}>
+        <Buttons addons=true alignment={*alignment}>
             { for T::iter().map(button_map) }
         </Buttons>
     }
