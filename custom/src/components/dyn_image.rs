461f5cd61d9612d588f72eca409917714003681b
thomas <thomas@dooms.eu>
2022-02-01 20:53:43 +0100

    class on dynimage

--- custom/src/components/dyn_image.rs
+++ custom/src/components/dyn_image.rs
@@ -7,11 +7,13 @@ pub struct Props {
 
     #[prop_or(100)]
     pub height: u32,
+
+    pub class: Classes,
 }
 
 #[function_component(DynImage)]
 pub fn dyn_image(props: &Props) -> Html {
     let style = format!("height:{}vh;background-image:url('{}');background-size:contain;background-repeat:no-repeat;background-position:center center;", props.height, props.src);
 
-    html! { <div style={style}> </div> }
+    html! { <div class={props.class.clone()} {style}> </div> }
 }
