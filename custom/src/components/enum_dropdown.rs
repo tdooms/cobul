2b555df04c6408d14fe884d50e8868b5adeb9080
thomas <thomas@dooms.eu>
2022-04-03 20:18:21 +0200

    strum update

--- custom/src/components/enum_dropdown.rs
+++ custom/src/components/enum_dropdown.rs
@@ -3,7 +3,7 @@ use yew::prelude::*;
 
 use base::components::{Dropdown, DropdownItem};
 use base::elements::{Button, Icon};
-use base::props::Color;
+use base::props::{Color, Size};
 
 // TODO: deselect when not in focus
 #[derive(Clone, Debug, Properties, PartialEq)]
@@ -14,6 +14,9 @@ pub struct Props<T: IntoEnumIterator + ToString + Copy + PartialEq + 'static> {
     pub value: T,
 
     pub onchange: Callback<T>,
+
+    #[prop_or_default]
+    pub size: Size,
 }
 
 #[function_component(EnumDropdown)]
@@ -27,6 +30,7 @@ where
         class,
         value,
         onchange,
+        size,
     } = &props;
 
     let trigger = {
@@ -34,10 +38,11 @@ where
         let onclick = Callback::from(move |_| active.set(!(*active)));
 
         html! {
-        <Button {onclick}>
-            <span> {value.to_string()} </span>
-            <Icon icon="fas fa-angle-down"/>
-        </Button>}
+            <Button {onclick} size={size.clone()}>
+                <span> {value.to_string()} </span>
+                <Icon icon="fas fa-angle-down"/>
+            </Button>
+        }
     };
 
     let dropdown_map = |variant: T| {
