02e72ff22e9864e86d9c31690237083919ad7c6d
thomas <thomas@dooms.eu>
2022-07-04 19:58:42 +0200

    modal delete didn't have props

--- base/src/components/modal/modal_card.rs
+++ base/src/components/modal/modal_card.rs
@@ -20,6 +20,9 @@ pub struct Props {
 
     #[prop_or_default]
     pub style: Option<String>,
+
+    #[prop_or_default]
+    pub onclose: Option<Callback<()>>,
 }
 
 /// [https://bulma.io/documentation/components/modal/](https://bulma.io/documentation/components/modal/)
@@ -32,13 +35,18 @@ pub fn modal_card(props: &Props) -> Html {
         None => html! {},
     };
 
+    let close = match props.onclose.as_ref().map(|x| x.reform(|_| ())) {
+        Some(onclick) => html! {<button class="delete" aria-label="close" {onclick}></button>},
+        None => html! {}
+    };
+
     html! {
         <div style={props.style.clone()} class={classes}>
             <div class="modal-background"></div>
             <div class="modal-card">
                 <header class="modal-card-head">
                     <p class="modal-card-title">{ props.title.clone() }</p>
-                    <button class="delete" aria-label="close"></button>
+                    {close}
                 </header>
                 <section class="modal-card-body">
                     { for props.children.iter() }
