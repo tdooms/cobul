1b1956877cc157bbed4c6d6e8534bc95d8c1a44f
thomas <thomas@dooms.eu>
2022-08-06 16:35:01 +0200

    pagination now has indices that are the same as shown

--- simple/src/pagination.rs
+++ simple/src/pagination.rs
@@ -38,19 +38,19 @@ pub fn pagination(props: &Props) -> Html {
 
     let ellipsis = html! {<PaginationEllipsis> {"\u{2026}"} </PaginationEllipsis>};
 
-    let left_ellipsis = (page >= 3).then(|| ellipsis.clone()).unwrap_or_default();
+    let left_ellipsis = (page >= 4).then(|| ellipsis.clone()).unwrap_or_default();
 
-    let right_ellipsis = (total - page >= 4).then(|| ellipsis).unwrap_or_default();
+    let right_ellipsis = (total - page >= 3).then(|| ellipsis).unwrap_or_default();
 
     let item = |idx| {
         let onclick = onchange.reform(move |_| idx);
-        html! { <PaginationLink {onclick} current={ page == idx }> {idx + 1} </PaginationLink> }
+        html! { <PaginationLink {onclick} current={ page == idx }> {idx} </PaginationLink> }
     };
 
-    let first = item(0);
-    let last = (total != 1).then(|| item(total - 1)).unwrap_or_default();
+    let first = item(1);
+    let last = (total != 1).then(|| item(total)).unwrap_or_default();
 
-    let center = (page.max(2) - 1)..(page + 2).min(total - 1);
+    let center = (page.max(3) - 1)..(page + 2).min(total);
 
     html! {
         <components::Pagination {size} {class} {alignment} {rounded}>
--- test/src/pagination.rs
+++ test/src/pagination.rs
@@ -4,7 +4,7 @@ use yew::*;
 
 #[function_component(PaginationTester)]
 pub fn pagination_tester() -> Html {
-    let state = use_state(|| 0);
+    let state = use_state(|| 1);
     let onchange = ywt::callback!(state; move |idx| state.set(idx));
 
     html! {
