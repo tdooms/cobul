a500125044fff6ab6a6fed64cf968c2528351768
thomas <thomas@dooms.eu>
2022-11-01 15:39:24 +0100

    oops

--- base/src/elements/image.rs
+++ base/src/elements/image.rs
@@ -27,17 +27,20 @@ pub struct Props {
 #[function_component(Image)]
 pub fn image(props: &Props) -> Html {
     let class = classes!("image", props.class.clone(), props.size);
-    let has_placeholder = props.placeholder.is_some();
 
     let loaded = use_state(|| false);
-    let onload = callback!(loaded; move |_| loaded.set(true));
+
+    let onload = {
+        let cloned = loaded.clone();
+        Callback::from(move |_| cloned.set(true))
+    };
 
     let display = ["none", ""];
 
     html! {
         <figure style={props.style.clone()} {class}>
-            <img class={ classes!(props.rounded) } src={ props.src.clone() } {onload} display={display[loaded as usize]}/>
-            <img class={ classes!(props.rounded) } src={ props.placeholder.clone() } display={display[!loaded as usize]} />
+            <img class={ classes!(props.rounded) } src={ props.src.clone() } {onload} display={display[*loaded as usize]}/>
+            <img class={ classes!(props.rounded) } src={ props.placeholder.clone() } display={display[!*loaded as usize]} />
         </figure>
     }
 }
